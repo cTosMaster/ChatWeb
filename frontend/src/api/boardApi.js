bd1398b57e0442f073dea464c219b2e577605a71
Jaybee <asd1213719@gmail.com>
2025-05-07 12:50:49 +0900

    #boardApi-description#

--- frontend/src/api/boardApi.js
+++ frontend/src/api/boardApi.js
@@ -3,13 +3,14 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:3001/api/board";
 
+//게시글 페이지 조회
 export const fetchPostList = (page = 1, searchQuery = "") => {
   const queryParam = searchQuery ? `&search=${encodeURIComponent(searchQuery)}` : "";
   return axios.get(`${BASE_URL}/pages?page=${page}${queryParam}`)
               .then(res => res.data.data);
 };
 
-// 게시글 상세 조회
+// 게시글 id 단일 조회
 export const fetchPostById = (id) =>
   axios.get(`${BASE_URL}/${id}`).then((res) => res.data);
 
