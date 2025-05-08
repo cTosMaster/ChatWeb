3979ad5477bb9449854f193a601bd75f141befd1
Jaybee <asd1213719@gmail.com>
2025-05-08 15:26:35 +0900

    #게시판-v1.0#

--- frontend/src/pages/BoardMainPage.jsx
+++ frontend/src/pages/BoardMainPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { fetchPostList } from "../api/boardApi";
-import { useNavigate , Link} from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import '../css/BoardMainPage.css';
 import AuthCodeInput from "../security/AuthCodeInput";
 import { Form, Button, Row, Col, Pagination } from 'react-bootstrap';
@@ -16,31 +16,31 @@ const BoardMainPage = () => {
   const [hasNextPage, setHasNextPage] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");       // 검색어 상태 추가
 
-    // 인증 처리 부분
-    const [isVerified, setIsVerified] = useState(false);
-    const [showAuthModal, setShowAuthModal] = useState(false);
+  // 인증 처리 부분
+  const [isVerified, setIsVerified] = useState(false);
+  const [showAuthModal, setShowAuthModal] = useState(false);
 
 
-    useEffect(() => {
-      const verified = sessionStorage.getItem("authCodeVerified");
-      if (verified === "true") {
-        setIsVerified(true);
-      }
-    }, []);
+  useEffect(() => {
+    const verified = sessionStorage.getItem("authCodeVerified");
+    if (verified === "true") {
+      setIsVerified(true);
+    }
+  }, []);
 
-    useEffect(() => {
-      if (!isVerified) return;
-      fetchPostList(page, searchQuery)
-        .then(data => {
-          console.log("받은 데이터:", data);
-          setPosts(data);
-          setHasNextPage(data.length === 10);
-        })
-        .catch(err => console.error('목록 불러오기 실패', err));
-    }, [page, searchQuery, isVerified]);
+  useEffect(() => {
+    if (!isVerified) return;
+    fetchPostList(page, searchQuery)
+      .then(data => {
+        console.log("받은 데이터:", data);
+        setPosts(data);
+        setHasNextPage(data.length === 10);
+      })
+      .catch(err => console.error('목록 불러오기 실패', err));
+  }, [page, searchQuery, isVerified]);
 
-    const goNext = () => setPage(prev => prev + 1);
-    const goPrev = () => setPage(prev => (prev > 1 ? prev - 1 : 1));
+  const goNext = () => setPage(prev => prev + 1);
+  const goPrev = () => setPage(prev => (prev > 1 ? prev - 1 : 1));
 
 
   //페이지 출력 처리
@@ -58,80 +58,100 @@ const BoardMainPage = () => {
     setPage(1); // 인증되면 페이지 리셋
   };
 
+  // !!! 등록 버튼 핸들러
+  const handleBoardPage = () => {
+    navigate("/boardpage");
+  };
+
   return (
     <div className="board-container" >
       <h1 className="board-title">노티스텔</h1>
       <p className="board-desc">우리 집에 대한 따끈한 소식이 있어요!</p>
       <div style={{ maxWidth: '800px', margin: '0 auto', marginBottom: '16px' }}>
         {/* 인증 버튼 - 우측 상단 정렬 */}
-                <Row className="mb-2">
-                  <Col>
-                    <div className="d-flex justify-content-end">
-                      {isVerified ? (
-                        <Button variant="success" style={{width: '80px', marginRight:'12px'}} disabled>인증됨</Button>
-                      ) : (
-                        <Button variant="warning" onClick={handleOpenAuth} style={{width: '120px', marginRight:'12px'}}  >인증코드 입력</Button>
-                      )}
-                    </div>
-                  </Col>
-                </Row>
-    <Row className="justify-content-end" style={{ marginRight: '0', marginLeft: '0', gap: '8px' }}>
-        <Col xs="auto" style={{ paddingRight: '0px' }}>
-            <Form.Control
-                type="text"
-                placeholder="게시글 검색"
-                value={searchQuery}
-                onChange={handleSearchChange}
-                style={{ minWidth: '200px', borderRadius: '8px', padding: '8px' }}
-            />
-        </Col>
-        <Col xs="auto" style={{ paddingLeft: '0px' }}>
-            <Button 
-                type="submit" 
-                variant="primary" 
-                style={{ minWidth: '80px', padding: '8px', borderRadius: '8px' }}
+        <Row className="mb-2">
+          <Col>
+            <div className="d-flex justify-content-end">
+              {isVerified ? (
+                <Button variant="success" style={{ width: '80px', marginRight: '12px' }} disabled>인증됨</Button>
+              ) : (
+                <Button variant="warning" onClick={handleOpenAuth} style={{ width: '120px', marginRight: '12px' }}  >인증코드 입력</Button>
+              )}
+            </div>
+          </Col>
+        </Row>
+        <Row className="justify-content-end" style={{ marginRight: '0', marginLeft: '0', gap: '8px' }}>
+          {/* !!! 등록 버튼 */}
+          <Col xs="auto" style={{ paddingLeft: "0px", flex: 1 }}>
+            <Button
+              onClick={handleBoardPage}
+              className="submit"
+              style={{
+                minWidth: "120px",
+                padding: "8px",
+                borderRadius: "8px",
+                marginLeft: "0",
+              }}
             >
-                검색
+              게시글 등록
             </Button>
-        </Col>
-    </Row>
+          </Col>
+          <Col xs="auto" style={{ paddingRight: '0px' }}>
+            <Form.Control
+              type="text"
+              placeholder="게시글 검색"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              style={{ minWidth: '200px', borderRadius: '8px', padding: '8px' }}
+            />
+          </Col>
+          <Col xs="auto" style={{ paddingLeft: '0px' }}>
+            <Button
+              type="submit"
+              variant="primary"
+              style={{ minWidth: '80px', padding: '8px', borderRadius: '8px' }}
+            >
+              검색
+            </Button>
+          </Col>
+        </Row>
       </div>
       {!isVerified ? (
         <p className="text-center text-danger">인증되지 않은 사용자입니다.</p>
       ) : posts.length === 0 ? (
         <p className="text-center">게시글이 없습니다.</p>
       ) : (
-      <table className="board-table">
-        <thead>
-          <tr>
-            <th style={{width: '45%'}}>제목</th>
-            <th style={{width: '164px'}}>작성자</th>
-            <th style={{width: '160px'}}>작성일자</th>
-            <th style={{width: '120px'}}>조회수</th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map(post => (
-          <tr key={post.id} style={{ cursor: 'pointer' }}>
-            <Link 
-                to={`/post/${post.id}`} 
-                state={{post}} 
-                style={{ 
-                    display: 'contents', 
-                    textDecoration: 'none', 
-                    color: 'inherit' 
-                }}
-            >
-                <td style={{ textAlign: 'center' }}>{post.title}</td>
-                <td>{post.writer}</td>
-                <td>{new Date(post.created_at).toISOString().split('T')[0]}</td>
-                <td>{post.view_cnt}</td>
-            </Link>
-    </tr>
-          ))}
+        <table className="board-table">
+          <thead>
+            <tr>
+              <th style={{ width: '45%' }}>제목</th>
+              <th style={{ width: '164px' }}>작성자</th>
+              <th style={{ width: '160px' }}>작성일자</th>
+              <th style={{ width: '120px' }}>조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map(post => (
+              <tr key={post.id} style={{ cursor: 'pointer' }}>
+                <Link
+                  to={`/post/${post.id}`}
+                  state={{ post }}
+                  style={{
+                    display: 'contents',
+                    textDecoration: 'none',
+                    color: 'inherit'
+                  }}
+                >
+                  <td style={{ textAlign: 'center' }}>{post.title}</td>
+                  <td>{post.writer}</td>
+                  <td>{new Date(post.created_at).toISOString().split('T')[0]}</td>
+                  <td>{post.view_cnt}</td>
+                </Link>
+              </tr>
+            ))}
 
-        </tbody>
-      </table>
+          </tbody>
+        </table>
       )}
 
 
